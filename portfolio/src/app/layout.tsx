196dbb7250b1cc162083107ac10e9d1e68539567
shimizu <noon3m.op.sh@gmail.com>
2024-02-02 09:39:39 +0900

    [add]ファビコンの更新

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css"
+import Favicon from '../../public/rabbit.jpg';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Portfolio",
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
